d666ea384e2da171f07d155a2b2f5bb684837e05
gabyitt <gabi2008ok@gmail.com>
2026-01-09 19:07:12 -0300

    Operadores de atribuição

---
+++ Course/Aula3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo1();
+            Exemplo2();
+        }
+        static void Exemplo1()
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+        }
+        static void Exemplo2()
+        {
+            int a = 10;
+            int b = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
