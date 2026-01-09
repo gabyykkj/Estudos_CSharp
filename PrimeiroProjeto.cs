6c69febb31360ffbecd36997dbaf6102954dec22
gabyitt <gabi2008ok@gmail.com>
2026-01-09 01:49:35 -0300

    Primeiro commit

---
+++ PrimeiroProjeto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PrimeiroProjeto
+{
+    class PrimeiroProjeto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
