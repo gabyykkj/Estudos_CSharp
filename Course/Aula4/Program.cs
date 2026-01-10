72d55942545d855d0e6cccf8856d6c3fca35d317
gabyitt <gabi2008ok@gmail.com>
2026-01-10 01:21:06 -0300

    Operadores aritméticos

---
+++ Course/Aula4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo1();
+            Exemplo2();
+        }
+        static void Exemplo1()
+        {
+            float x = 4.5f;
+            double y = x;
+
+            Console.WriteLine(y);
+
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b);
+        }
+        static void Exemplo2()
+        {
+            int a = 5;
+            int b = 2;
+
+            double resultado = (double) a / b;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
