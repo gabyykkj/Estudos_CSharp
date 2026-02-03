98b6ab478d44549a3c526a0c19cf03c2b5762cac
gabyitt <gabi2008ok@gmail.com>
2026-02-03 20:04:10 -0300

    Entendendo encapsulamento

---
+++ Course/Aula19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            Produto p = new Produto("TV", 500.00, 10);
+            
+            Console.WriteLine(p.GetNome());
+            
+            p.SetNome("T");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+        }
+    }
+}
