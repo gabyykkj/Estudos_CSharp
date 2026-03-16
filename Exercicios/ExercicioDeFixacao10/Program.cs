4538b96773ba8082305bd4f865ab2f06dc1224d9
gabyitt <gabi2008ok@gmail.com>
2026-03-16 20:31:34 -0300

    Exercício de pirâmide

---
+++ Exercicios/ExercicioDeFixacao10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioDeFixacao10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o tamanho da pirâmide: ");
+            int tamanho = int.Parse(Console.ReadLine());
+            int inicio = 1;
+
+            for( int i = tamanho; i != 0; i--)
+            {
+                string resultado = new String(' ', i);
+                string pontos = new String('*', inicio);
+                Console.WriteLine(resultado + pontos);
+
+                inicio += 2;
+            }
+        }
+    }
+}
