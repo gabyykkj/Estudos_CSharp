675be32c0df15ee952ecea5513b13b59a522bf69
gabyitt <gabi2008ok@gmail.com>
2026-03-18 22:35:55 -0300

    Ordenando uma array

---
+++ Exercicios/ExercicioDeFixacao11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ExercicioDeFixacao11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 5, 2, 7, 3, 1, 4, 8, 9, 6 };
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                for (int j = 0; j < numeros.Length; j++)
+                {
+                    if (numeros[i] < numeros[j])
+                    {
+                        int troca1 = numeros[i];
+                        int troca2 = numeros[j];
+
+                        numeros[i] = troca2;
+                        numeros[j] = troca1;
+                    }
+                }
+                foreach (int numero in numeros)
+                {
+                    string x = new String('#', numero);
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine("----------------------------------------");
+            }
+
+        }
+    }
+}
